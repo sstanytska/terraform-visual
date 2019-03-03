60c6c18d8cb67c7c3346a17748a1c939f6e0ed03
sstanytska <stanytskasofia@gmail.com>
2019-03-03 15:41:03 -0600

    changed instance2

--- instance.tf
+++ instance.tf
@@ -3,5 +3,5 @@ resource "aws_instance" "example" {
   instance_type = "${var.instance_type}"
   key_name      = "${var.key_name}"
   subnet_id     = "${var.subnet_id}"
-  associate_public_ip_address = "true"
+  associate_public_ip_address = true
 }
