123abd2bc878fa6c5459216eea7c058bba30ca15
sstanytska <stanytskasofia@gmail.com>
2019-03-03 16:06:53 -0600

    changed file

--- asg.tf
+++ asg.tf
@@ -1,6 +1,6 @@
 resource "aws_autoscaling_group" "dev" {
     name = "dev"
-    launch_configuration = "${var.aws_autoscaling_group.dev.name}"
+    launch_configuration = "${aws_launch_configuration.Dev.name}"
     min = "${var.min_size}"
     max = "${var.max_size}"
   
