247f06fdb2fa5def39e70129ceafe72f778355e7
sstanytska <stanytskasofia@gmail.com>
2019-03-06 18:46:24 -0600

    add chenged

--- asg.tf
+++ asg.tf
@@ -1,6 +1,6 @@
 resource "aws_autoscaling_group" "dev" {
     name = "dev"
-    launch_configuration = "${aws_launch_configuration.Dev.name}"
+    launch_configuration = "${aws_launch_configuration.dev.name}"
     min_size = "${var.min_size}"
     max_size = "${var.max_size}"
     vpc_zone_identifier = ["${aws_subnet.dev1.id}", "${aws_subnet.dev2.id}"]
