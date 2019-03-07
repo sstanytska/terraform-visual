f84c3849c2c991d8d8deff6db0cb5adab02b70e1
sstanytska <stanytskasofia@gmail.com>
2019-03-06 18:33:18 -0600

    changes

--- asg.tf
+++ asg.tf
@@ -3,5 +3,5 @@ resource "aws_autoscaling_group" "dev" {
     launch_configuration = "${aws_launch_configuration.Dev.name}"
     min_size = "${var.min_size}"
     max_size = "${var.max_size}"
-    vpc_zone_identifier = ["${aws_subnet.dev1.id}", "${aws_subnet.dev2}"]
+    vpc_zone_identifier = ["${aws_subnet.dev1.id}", "${aws_subnet.dev2.id}"]
 }
