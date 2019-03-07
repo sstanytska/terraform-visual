eaed2db6e0bf130248fb231706864935b14a1213
sstanytska <stanytskasofia@gmail.com>
2019-03-06 18:38:28 -0600

    changed ls

--- lc.tf
+++ lc.tf
@@ -2,6 +2,6 @@ resource "aws_launch_configuration" "Dev" {
     name = "dev"
     image_id = "${var.ami}"
     instance_type = "${var.instance_type}"
-  
+    associate_public_ip_address = true 
 }
 
